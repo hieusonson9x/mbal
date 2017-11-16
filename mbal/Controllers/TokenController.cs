05d763ebf9a0dbcbf74210ad48994bf086da0eb8
hieu <hieusonson9x@gmail.com>
2017-11-17 00:40:01 +0700

    cấu hình xác thực ws

---
+++ mbal/Controllers/TokenController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mbal.Models;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+
+namespace mbal.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Token")]
+    public class TokenController : Controller
+    {
+        private readonly UserContext _context;
+
+        public TokenController(UserContext context)
+        {
+            _context = context;
+        }
+        [AllowAnonymous]
+        [HttpPost("get")]
+        public IActionResult Create([FromBody] User inputModel)
+        {
+            var claims = new[]
+             {
+              new Claim(JwtRegisteredClaimNames.Sub, inputModel.username),
+              new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Fiver.Security.Bearer"));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken("Fiver.Security.Bearer",
+                  "Fiver.Security.Bearer",
+                  claims,
+                  expires: DateTime.Now.AddMinutes(30),
+                  signingCredentials: creds);
+
+                return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+        }
+
+        // GET: api/Token
+        [Authorize]
+        [HttpGet]
+        public IEnumerable<User> Getusers()
+        {
+            return _context.users;
+        }
+
+        // GET: api/Token/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _context.users.SingleOrDefaultAsync(m => m.id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // PUT: api/Token/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser([FromRoute] long id, [FromBody] User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Token
+        [HttpPost]
+        public async Task<IActionResult> PostUser([FromBody] User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.id }, user);
+        }
+
+        // DELETE: api/Token/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _context.users.SingleOrDefaultAsync(m => m.id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        private bool UserExists(long id)
+        {
+            return _context.users.Any(e => e.id == id);
+        }
+    }
+}
--- mbal/Startup.cs
+++ mbal/Startup.cs
@@ -2,6 +2,10 @@
 using Microsoft.Extensions.DependencyInjection;
 using mbal.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using mbal.Secrete;
 
 namespace mbal
 {
@@ -9,17 +13,36 @@ namespace mbal
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
+            
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                  .AddJwtBearer(cfg =>
+                  {
+                      cfg.RequireHttpsMetadata = false;
+                      cfg.SaveToken = true;
+
+                      cfg.TokenValidationParameters = new TokenValidationParameters()
+                      {
+                          ValidIssuer = "Fiver.Security.Bearer",
+                          ValidAudience = "Fiver.Security.Bearer",
+                          IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Fiver.Security.Bearer"))
+                      };
+
+                  });
+
+                services.AddMvc()
                 .AddJsonOptions(
                 options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-            ); 
-            var connection = @"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.AspNetCore.NewDb;Trusted_Connection=True;";
+            );
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=MBAL;Trusted_Connection=True;";
             services.AddDbContext<UserContext>(options => options.UseSqlServer(connection));
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseAuthentication();
             app.UseMvc();
+            
         }
     }
 }
